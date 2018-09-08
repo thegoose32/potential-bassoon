665647e4bfc61808fe732d9d1c8e75bb03cf80fd
Paul Giese <giese.paul@gmail.com>
2018-09-08 10:47:13 -0400

    fixed revenue forecast

--- static/model.js
+++ static/model.js
@@ -737,7 +737,7 @@ export function calculateFcstRevenueV2(revenueMilestones, blankRevArray, fcstExp
   let revArray = blankRevArray.map((period, periodIndex) => {
     let milestoneEarnedPeriod = milestoneRevEarned.filter(milestonePeriod => milestonePeriod.period === period.period);
     let cummFcstExpPeriod = percentComplete.filter(cummFcstPeriod => cummFcstPeriod.period === period.period);
-    if (period.period === versionPeriod + 0.25 && activeVersionID !== 0) {
+    if (period.period === versionPeriod && activeVersionID !== 0) {
       period.amount = (milestoneEarnedPeriod[0].amount * cummFcstExpPeriod[0].amount) - revenueThruPeriod;
     } else {
       period.amount = (milestoneEarnedPeriod[0].amount * cummFcstExpPeriod[0].amount);
