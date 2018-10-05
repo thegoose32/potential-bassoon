d42ba32b024cda5187db41b840a41b036ecd9aa7
David Giese <johndgiese@gmail.com>
2018-10-05 14:26:50 -0400

    Fix import bug

--- server/common/tests/conftest.py
+++ server/common/tests/conftest.py
@@ -1,11 +1,11 @@
 import pytest
-import selenium
+from selenium.webdriver import Chrome
 
 import chromedriver_binary  # adds chromedriver to the path
 
 @pytest.fixture
 def driver(live_server):
-    driver = selenium.webdriver.Chrome()
+    driver = Chrome()
     driver.get(live_server.url)
     yield driver
     driver.close()
